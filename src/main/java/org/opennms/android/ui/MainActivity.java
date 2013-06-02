f3b28e6b036d33f0293d7f91e97ecb8021731caf
Roman Tsukanov <roman@tsukanov.me>
2013-06-02 20:42:13 +1000

    Now using Maven to simplify build management. Changed project structure. Removed ActionBarSherlock and Test modules. Removed unused libraries.

---
+++ src/main/java/org/opennms/android/ui/MainActivity.java
@@ -0,0 +1,224 @@
+package org.opennms.android.ui;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+import org.opennms.android.R;
+import org.opennms.android.service.RefreshService;
+import org.opennms.android.ui.alarms.AlarmsListFragment;
+import org.opennms.android.ui.events.EventsListFragment;
+import org.opennms.android.ui.nodes.NodesListFragment;
+import org.opennms.android.ui.outages.OutagesListFragment;
+
+public class MainActivity extends SherlockFragmentActivity {
+    public Intent refreshServiceIntent;
+    private DrawerLayout navigationLayout;
+    private ListView navigationList;
+    private ActionBarDrawerToggle navigationToggle;
+    private CharSequence title;
+    private String[] navigationItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        final CharSequence drawerTitle = title = getTitle();
+        navigationLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationList = (ListView) findViewById(R.id.left_navigation);
+        navigationItems = getResources().getStringArray(R.array.navigation_items);
+
+        navigationLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        navigationList.setAdapter(new ArrayAdapter<String>(this, R.layout.navigation_list_item, navigationItems));
+        navigationList.setOnItemClickListener(new DrawerItemClickListener());
+
+        // Enable ActionBar app icon to behave as action to toggle navigation
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        navigationToggle = new ActionBarDrawerToggle(
+                this,
+                navigationLayout,
+                R.drawable.ic_drawer,
+                R.string.drawer_open,
+                R.string.drawer_close
+        ) {
+            public void onDrawerClosed(View view) {
+                getSupportActionBar().setTitle(title);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                getSupportActionBar().setTitle(drawerTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        navigationLayout.setDrawerListener(navigationToggle);
+
+        refreshServiceIntent = new Intent(getApplicationContext(), RefreshService.class);
+
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        if (sharedPref.getBoolean("is_first_launch", true)) {
+            sharedPref.edit().putBoolean("is_first_launch", false).commit();
+            showWelcomeDialog();
+        }
+
+        if (savedInstanceState == null) {
+            selectItem(0);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getApplicationContext().startService(refreshServiceIntent);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getApplicationContext().stopService(refreshServiceIntent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getSupportMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        boolean isDrawerOpen = navigationLayout.isDrawerOpen(navigationList);
+        // TODO: Hide menu items
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                if (navigationLayout.isDrawerOpen(navigationList)) {
+                    navigationLayout.closeDrawer(navigationList);
+                } else {
+                    navigationLayout.openDrawer(navigationList);
+                }
+                return true;
+            case R.id.menu_settings:
+                Intent settingsIntent = new Intent(getApplicationContext(),
+                        SettingsActivity.class);
+                startActivity(settingsIntent);
+                return true;
+            case R.id.menu_about:
+                showAboutDialog();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void selectItem(int position) {
+        Fragment fragment;
+        switch (position) {
+            case 0:
+                fragment = new NodesListFragment();
+                break;
+            case 1:
+                fragment = new OutagesListFragment();
+                break;
+            case 2:
+                fragment = new EventsListFragment();
+                break;
+            case 3:
+                fragment = new AlarmsListFragment();
+                break;
+            default:
+                return;
+        }
+        getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, fragment).commit();
+
+        // update selected item and title, then close the drawer
+        navigationList.setItemChecked(position, true);
+        setTitle(navigationItems[position]);
+        navigationLayout.closeDrawer(navigationList);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        this.title = title;
+        getSupportActionBar().setTitle(this.title);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        navigationToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        navigationToggle.onConfigurationChanged(newConfig);
+    }
+
+    private void showAboutDialog() {
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = inflater.inflate(R.layout.about, (ViewGroup) findViewById(R.layout.about));
+        AlertDialog.Builder builder = new AlertDialog.Builder(this).setView(layout).setNeutralButton(
+                getString(R.string.close_dialog),
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        builder.create().show();
+    }
+
+    private void showWelcomeDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                .setMessage(getResources().getString(R.string.welcome_message))
+                .setPositiveButton(
+                        getResources().getString(R.string.welcome_message_pos_button),
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Intent settingsIntent = new Intent(getApplicationContext(), SettingsActivity.class);
+                                startActivity(settingsIntent);
+                            }
+                        })
+                .setNegativeButton(
+                        getResources().getString(R.string.welcome_message_neg_button),
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        })
+                .setCancelable(false);
+        AlertDialog aboutDialog = builder.create();
+        aboutDialog.show();
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+}
