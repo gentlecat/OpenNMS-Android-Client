3330d4306e1ac3f54871feb8df2ca6d0c745e1a7
Roman Tsukanov <roman@tsukanov.me>
2013-06-05 23:05:56 +1000

    Fixed #4 and some other things.

--- tests/src/main/java/org/opennms/android/tests/ui/MainActivityTest.java
+++ tests/src/main/java/org/opennms/android/tests/ui/MainActivityTest.java
@@ -23,6 +23,11 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
         super.setUp();
         activity = getActivity();
         solo = new Solo(getInstrumentation(), activity);
+
+        if (solo.searchText(solo.getString(R.string.welcome_message)))
+        {
+            solo.clickOnButton(solo.getString(R.string.welcome_message_neg_button));
+        }
     }
 
     public void testAboutDialog() throws Exception {
@@ -30,18 +35,23 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
 
         solo.clickOnMenuItem(solo.getString(R.string.about), true);
         assertTrue("About dialog text not found", solo.searchText(solo.getString(R.string.about_info)));
+        Spoon.screenshot(activity, "dialog_opened");
 
         Button dialogCloseButton = (Button) solo.getView(android.R.id.button3);
         solo.clickOnButton(solo.getString(R.string.close_dialog));
         solo.sleep(200);
+        Spoon.screenshot(activity, "dialog_closed");
 
         ArrayList<View> currentViews = solo.getCurrentViews();
         assertFalse("Not returned to MainActivity", currentViews.contains(dialogCloseButton));
     }
 
     public void testSettings() throws Exception {
+        Spoon.screenshot(activity, "initial_state");
+
         solo.clickOnMenuItem(solo.getString(R.string.settings), true);
         assertTrue("SettingsActivity was not found", solo.waitForActivity("SettingsActivity"));
+        Spoon.screenshot(activity, "settings_activity_opened");
     }
 
     @Override
