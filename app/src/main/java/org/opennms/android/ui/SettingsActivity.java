80b5f66e7ca64f60519c8251fc5c05eefe0a113e
Roman Tsukanov <roman@tsukanov.me>
2013-06-06 02:43:54 +1000

    Added alarm notification settings.

--- app/src/main/java/org/opennms/android/ui/SettingsActivity.java
+++ app/src/main/java/org/opennms/android/ui/SettingsActivity.java
@@ -11,7 +11,7 @@ import org.opennms.android.R;
 
 public class SettingsActivity extends SherlockPreferenceActivity implements OnSharedPreferenceChangeListener {
 
-    private PreferenceManager prefManager;
+    SharedPreferences sharedPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,25 +20,14 @@ public class SettingsActivity extends SherlockPreferenceActivity implements OnSh
         actionBar.setTitle(getResources().getString(R.string.settings));
         actionBar.setDisplayHomeAsUpEnabled(true);
         addPreferencesFromResource(R.xml.settings);
-        prefManager = getPreferenceManager();
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
-        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
-        findPreference("host").setSummary(sharedPref.getString("host", getResources().getString(R.string.default_host)));
-        findPreference("port").setSummary(sharedPref.getString("port", Integer.toString(getResources().getInteger(R.integer.default_port))));
-        findPreference("path").setSummary(sharedPref.getString("path", getResources().getString(R.string.default_path)));
-        findPreference("user").setSummary(sharedPref.getString("user", getResources().getString(R.string.default_user)));
-        if (sharedPref.getBoolean("https", getResources().getBoolean(R.bool.default_https))) {
-            findPreference("https").setSummary(getResources().getString(R.string.settings_https_on));
-        } else {
-            findPreference("https").setSummary(getResources().getString(R.string.settings_https_off));
-        }
-
         getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+        updateSummaries();
     }
 
     @Override
@@ -59,17 +48,30 @@ public class SettingsActivity extends SherlockPreferenceActivity implements OnSh
     }
 
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        if (key.equals("host") || key.equals("path") || key.equals("user")) { // String cases
-            prefManager.findPreference(key).setSummary(sharedPreferences.getString(key, null));
-        } else if (key.equals("port")) { // Integer case
-            prefManager.findPreference(key).setSummary(sharedPreferences.getString(key, null));
-        } else if (key.equals("https")) { // Boolean case
-            if (sharedPreferences.getBoolean(key, false)) {
-                prefManager.findPreference(key).setSummary(getResources().getString(R.string.settings_https_on));
-            } else {
-                prefManager.findPreference(key).setSummary(getResources().getString(R.string.settings_https_off));
-            }
+        updateSummaries();
+    }
+
+    private void updateSummaries() {
+        findPreference("user").setSummary(sharedPref.getString("user", getResources().getString(R.string.default_user)));
+
+        findPreference("host").setSummary(sharedPref.getString("host", getResources().getString(R.string.default_host)));
+        findPreference("port").setSummary(sharedPref.getString("port", Integer.toString(getResources().getInteger(R.integer.default_port))));
+        findPreference("path").setSummary(sharedPref.getString("path", getResources().getString(R.string.default_path)));
+        if (sharedPref.getBoolean("https", getResources().getBoolean(R.bool.default_https))) {
+            findPreference("https").setSummary(getResources().getString(R.string.settings_https_on));
+        } else {
+            findPreference("https").setSummary(getResources().getString(R.string.settings_https_off));
         }
+
+        if (sharedPref.getBoolean("notifications_on", getResources().getBoolean(R.bool.default_notifications))) {
+            findPreference("notifications_on").setSummary(getResources().getString(R.string.settings_notifications_enabled_true));
+        } else {
+            findPreference("notifications_on").setSummary(getResources().getString(R.string.settings_notifications_enabled_false));
+        }
+
+        String refreshRateSummary = sharedPref.getString("refresh_rate", getResources().getString(R.string.default_refresh_rate))
+                + " " + getResources().getString(R.string.settings_refresh_rate_minutes);
+        findPreference("refresh_rate").setSummary(refreshRateSummary);
     }
 
 }
