050917492118850936776293e2a85e5fef76beed
Roman Tsukanov <roman@tsukanov.me>
2013-06-16 00:18:49 +1000

    Updated Robolectric dependency and added FEST. Added a couple of tests.

--- src/test/java/org/opennms/android/ui/MainActivityTest.java
+++ src/test/java/org/opennms/android/ui/MainActivityTest.java
@@ -1,19 +1,58 @@
 package org.opennms.android.ui;
 
+import android.content.Intent;
+import android.widget.ImageView;
+import android.widget.TextView;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.opennms.android.R;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.shadows.ShadowActivity;
+import org.robolectric.shadows.ShadowIntent;
+import org.robolectric.tester.android.view.TestMenuItem;
 
+import static org.fest.assertions.api.ANDROID.assertThat;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.robolectric.Robolectric.shadowOf;
 
 @RunWith(RobolectricTestRunner.class)
 public class MainActivityTest {
+    private MainActivity activity;
+
+    @Before
+    public void setUp() throws Exception {
+        activity = Robolectric.buildActivity(MainActivity.class).create().get();
+    }
 
     @Test
-    public void shouldHaveHappySmiles() throws Exception {
-        String appName = new MainActivity().getResources().getString(R.string.app_name);
-        assertThat(appName, equalTo("OpenNMS"));
+    public void shouldHaveCorrectAppName() throws Exception {
+        String appName = activity.getResources().getString(R.string.app_name);
+        assertTrue(appName.equals("OpenNMS"));
     }
+
+    @Test
+    public void aboutDialogShouldOpen() throws Exception {
+        activity.showAboutDialog();
+        TextView aboutText = (TextView) activity.findViewById(R.id.about_info);
+        assertThat(aboutText).isVisible();
+        assertThat(aboutText).containsText(R.string.about_info);
+        ImageView logo = (ImageView) activity.findViewById(R.id.opennms_logo);
+        assertThat(logo).isVisible();
+    }
+
+    @Test
+    public void settingsMenuItemShouldOpenNewActivity() throws Exception {
+        TestMenuItem settingsMenuItem = new TestMenuItem(R.id.menu_settings);
+        settingsMenuItem.click();
+
+        ShadowActivity shadowActivity = shadowOf(activity);
+        Intent startedIntent = shadowActivity.getNextStartedActivity();
+        ShadowIntent shadowIntent = shadowOf(startedIntent);
+        assertThat(shadowIntent.getComponent().getClassName(), equalTo(SettingsActivity.class.getName()));
+    }
+
 }
