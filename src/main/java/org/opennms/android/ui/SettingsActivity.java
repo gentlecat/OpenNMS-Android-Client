70c42c6a7aca6a912fd38401facdc20f15ae93c9
Roman Tsukanov <roman@tsukanov.me>
2013-06-18 06:21:38 +1000

    Secondary notifications preferences are now disabled if notifications are off.

--- src/main/java/org/opennms/android/ui/SettingsActivity.java
+++ src/main/java/org/opennms/android/ui/SettingsActivity.java
@@ -52,8 +52,10 @@ public class SettingsActivity extends SherlockPreferenceActivity implements OnSh
     }
 
     private void updateSummaries() {
+        // Authentication
         findPreference("user").setSummary(sharedPref.getString("user", getResources().getString(R.string.default_user)));
 
+        // Server
         findPreference("host").setSummary(sharedPref.getString("host", getResources().getString(R.string.default_host)));
         findPreference("port").setSummary(sharedPref.getString("port", Integer.toString(getResources().getInteger(R.integer.default_port))));
         findPreference("path").setSummary(sharedPref.getString("path", getResources().getString(R.string.default_path)));
@@ -63,15 +65,22 @@ public class SettingsActivity extends SherlockPreferenceActivity implements OnSh
             findPreference("https").setSummary(getResources().getString(R.string.settings_https_off));
         }
 
-        if (sharedPref.getBoolean("notifications_on", getResources().getBoolean(R.bool.default_notifications))) {
+        // Notifications
+        Boolean notificationsOn = sharedPref.getBoolean("notifications_on", getResources().getBoolean(R.bool.default_notifications));
+        setNotificationPrefsEnabled(notificationsOn);
+        if (notificationsOn) {
             findPreference("notifications_on").setSummary(getResources().getString(R.string.settings_notifications_enabled_true));
         } else {
             findPreference("notifications_on").setSummary(getResources().getString(R.string.settings_notifications_enabled_false));
         }
-
         String refreshRateSummary = sharedPref.getString("refresh_rate", getResources().getString(R.string.default_refresh_rate))
                 + " " + getResources().getString(R.string.settings_refresh_rate_minutes);
         findPreference("refresh_rate").setSummary(refreshRateSummary);
+    }
+
+    void setNotificationPrefsEnabled(Boolean enabled) {
+        findPreference("wifi_only").setEnabled(enabled);
+        findPreference("refresh_rate").setEnabled(enabled);
     }
 
 }
