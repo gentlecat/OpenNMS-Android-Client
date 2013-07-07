8ea36fbe20e40df5f573af3672bef7291f202abb
Roman Tsukanov <roman@tsukanov.me>
2013-07-06 21:21:21 -0700

    Removed EventsServerCommunication interface.

--- src/main/java/org/opennms/android/communication/events/EventsServerCommunication.java
+++ src/main/java/org/opennms/android/communication/events/EventsServerCommunication.java
@@ -1,13 +1,35 @@
 package org.opennms.android.communication.events;
 
+import android.content.Context;
+import com.google.resting.component.impl.ServiceResponse;
+import org.opennms.android.communication.ServerCommunication;
 import org.opennms.android.dao.events.Event;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
-public interface EventsServerCommunication {
+public class EventsServerCommunication {
 
-    List<Event> getEvents(String url) throws InterruptedException, ExecutionException, IOException;
+    private final Context appContext;
+
+    public EventsServerCommunication(Context appContext) {
+        this.appContext = appContext;
+    }
+
+    public ArrayList<Event> getEvents(String url) throws InterruptedException, ExecutionException, IOException {
+        final ExecutorService executorService = Executors.newCachedThreadPool();
+        Future<ServiceResponse> eventsCommunication = executorService
+                .submit(new ServerCommunication("events?orderBy=id&order=desc", appContext));
+        ServiceResponse events = eventsCommunication.get();
+        ArrayList<Event> eventsList = new ArrayList<Event>();
+        if (events != null) {
+            eventsList = EventsParser.parse(events.getContentData().getContentInString());
+        }
+        return eventsList;
+    }
 
 }
