2c16d5e5c8f39d2ed6167d97bdbddd93c5f996d7
Roman Tsukanov <roman@tsukanov.me>
2013-07-17 05:27:56 +1000

    Removed Event class. No longer creating unnessasary object.

--- src/main/java/org/opennms/android/ui/events/EventDetailsFragment.java
+++ src/main/java/org/opennms/android/ui/events/EventDetailsFragment.java
@@ -12,7 +12,6 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import com.actionbarsherlock.app.SherlockFragment;
 import org.opennms.android.R;
-import org.opennms.android.dao.Event;
 import org.opennms.android.provider.Contract;
 
 import java.text.ParseException;
@@ -21,7 +20,6 @@ import java.util.Date;
 
 public class EventDetailsFragment extends SherlockFragment {
     private static final String TAG = "EventDetailsFragment";
-    Event event;
     private long eventId;
 
     // Do not remove
@@ -40,34 +38,37 @@ public class EventDetailsFragment extends SherlockFragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        updateContent();
+        updateContent(eventId);
     }
 
-    public void updateContent() {
-        event = getEvent(eventId);
-
-        if (event != null) {
+    public void updateContent(long eventId) {
+        Cursor cursor = getActivity().getContentResolver().query(
+                Uri.withAppendedPath(Contract.Events.CONTENT_URI, String.valueOf(eventId)),
+                null, null, null, null);
+        if (cursor.moveToFirst()) {
             // Event ID
-            TextView id = (TextView) getActivity().findViewById(R.id.event_id);
-            id.setText(getString(R.string.event_details_id) + event.getId());
+            int id = cursor.getInt(cursor.getColumnIndexOrThrow(Contract.Events._ID));
+            TextView idView = (TextView) getActivity().findViewById(R.id.event_id);
+            idView.setText(getString(R.string.event_details_id) + id);
 
             // Severity
-            TextView severity = (TextView) getActivity().findViewById(R.id.event_severity);
-            severity.setText(String.valueOf(event.getSeverity()));
+            String severity = cursor.getString(cursor.getColumnIndexOrThrow(Contract.Events.SEVERITY));
+            TextView severityView = (TextView) getActivity().findViewById(R.id.event_severity);
+            severityView.setText(String.valueOf(severity));
             LinearLayout severityRow = (LinearLayout) getActivity().findViewById(R.id.event_severity_row);
-            if (event.getSeverity().equals("CLEARED")) {
+            if (severity.equals("CLEARED")) {
                 severityRow.setBackgroundColor(getResources().getColor(R.color.severity_cleared));
-            } else if (event.getSeverity().equals("MINOR")) {
+            } else if (severity.equals("MINOR")) {
                 severityRow.setBackgroundColor(getResources().getColor(R.color.severity_minor));
-            } else if (event.getSeverity().equals("NORMAL")) {
+            } else if (severity.equals("NORMAL")) {
                 severityRow.setBackgroundColor(getResources().getColor(R.color.severity_normal));
-            } else if (event.getSeverity().equals("INDETERMINATE")) {
+            } else if (severity.equals("INDETERMINATE")) {
                 severityRow.setBackgroundColor(getResources().getColor(R.color.severity_minor));
-            } else if (event.getSeverity().equals("WARNING")) {
+            } else if (severity.equals("WARNING")) {
                 severityRow.setBackgroundColor(getResources().getColor(R.color.severity_warning));
-            } else if (event.getSeverity().equals("MAJOR")) {
+            } else if (severity.equals("MAJOR")) {
                 severityRow.setBackgroundColor(getResources().getColor(R.color.severity_major));
-            } else if (event.getSeverity().equals("CRITICAL")) {
+            } else if (severity.equals("CRITICAL")) {
                 severityRow.setBackgroundColor(getResources().getColor(R.color.severity_critical));
             }
 
@@ -75,10 +76,10 @@ public class EventDetailsFragment extends SherlockFragment {
             TextView timeTextView = (TextView) getActivity().findViewById(R.id.event_create_time);
             // Example: "2011-09-27T12:15:32.363-04:00"
             SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
-            String tileString = event.getCreateTime();
-            if (tileString != null) {
+            String createTimeString = cursor.getString(cursor.getColumnIndexOrThrow(Contract.Events.CREATE_TIME));
+            if (createTimeString != null) {
                 try {
-                    Date createTime = format.parse(event.getCreateTime());
+                    Date createTime = format.parse(createTimeString);
                     timeTextView.setText(createTime.toString());
                 } catch (ParseException e) {
                     Log.e(TAG, "Creation time parsing error");
@@ -86,55 +87,38 @@ public class EventDetailsFragment extends SherlockFragment {
             }
 
             // Log message
-            TextView logMessage = (TextView) getActivity().findViewById(R.id.event_log_message);
-            logMessage.setText(Html.fromHtml(event.getLogMessage()));
+            String logMessage = cursor.getString(cursor.getColumnIndexOrThrow(Contract.Events.LOG_MESSAGE));
+            TextView logMessageView = (TextView) getActivity().findViewById(R.id.event_log_message);
+            logMessageView.setText(Html.fromHtml(logMessage));
 
             // Description
-            TextView description = (TextView) getActivity().findViewById(R.id.event_desc);
-            description.setText(Html.fromHtml(event.getDescription()));
+            String description = cursor.getString(cursor.getColumnIndexOrThrow(Contract.Events.DESCRIPTION));
+            TextView descriptionView = (TextView) getActivity().findViewById(R.id.event_desc);
+            descriptionView.setText(Html.fromHtml(description));
 
             // Host
-            TextView host = (TextView) getActivity().findViewById(R.id.event_host);
-            host.setText(event.getHost());
+            String host = cursor.getString(cursor.getColumnIndexOrThrow(Contract.Events.HOST));
+            TextView hostView = (TextView) getActivity().findViewById(R.id.event_host);
+            hostView.setText(host);
 
             // IP address
-            TextView ipAddress = (TextView) getActivity().findViewById(R.id.event_ip_address);
-            ipAddress.setText(event.getIpAddress());
+            String ipAddress = cursor.getString(cursor.getColumnIndexOrThrow(Contract.Events.IP_ADDRESS));
+            TextView ipAddressView = (TextView) getActivity().findViewById(R.id.event_ip_address);
+            ipAddressView.setText(ipAddress);
 
             // Node
-            TextView nodeId = (TextView) getActivity().findViewById(R.id.event_node);
-            nodeId.setText(event.getNodeLabel() + " (#" + event.getNodeId() + ")");
+            int nodeId = cursor.getInt(cursor.getColumnIndexOrThrow(Contract.Events.NODE_ID));
+            String nodeLabel = cursor.getString(cursor.getColumnIndexOrThrow(Contract.Events.NODE_LABEL));
+            TextView nodeView = (TextView) getActivity().findViewById(R.id.event_node);
+            nodeView.setText(nodeLabel + " (#" + nodeId + ")");
 
-            // Service type
-            TextView serviceType = (TextView) getActivity().findViewById(R.id.event_service_type);
-            serviceType.setText(event.getServiceTypeName() + " (#" + event.getServiceTypeId() + ")");
-
-
-        }
-    }
-
-    private Event getEvent(long id) {
-        Cursor cursor = getActivity().getContentResolver().query(
-                Uri.withAppendedPath(Contract.Events.CONTENT_URI, String.valueOf(id)),
-                null, null, null, null
-        );
-        if (cursor.moveToFirst()) {
-            Event event = new Event(cursor.getInt(cursor.getColumnIndexOrThrow(Contract.Events._ID)));
-            event.setSeverity(cursor.getString(cursor.getColumnIndexOrThrow(Contract.Events.SEVERITY)));
-            event.setLogMessage(cursor.getString(cursor.getColumnIndexOrThrow(Contract.Events.LOG_MESSAGE)));
-            event.setDescription(cursor.getString(cursor.getColumnIndexOrThrow(Contract.Events.DESCRIPTION)));
-            event.setHost(cursor.getString(cursor.getColumnIndexOrThrow(Contract.Events.HOST)));
-            event.setIpAddress(cursor.getString(cursor.getColumnIndexOrThrow(Contract.Events.IP_ADDRESS)));
-            event.setCreateTime(cursor.getString(cursor.getColumnIndexOrThrow(Contract.Events.CREATE_TIME)));
-            event.setNodeId(cursor.getInt(cursor.getColumnIndexOrThrow(Contract.Events.NODE_ID)));
-            event.setNodeLabel(cursor.getString(cursor.getColumnIndexOrThrow(Contract.Events.NODE_LABEL)));
-            event.setServiceTypeId(cursor.getInt(cursor.getColumnIndexOrThrow(Contract.Events.SERVICE_TYPE_ID)));
-            event.setServiceTypeName(cursor.getString(cursor.getColumnIndexOrThrow(Contract.Events.SERVICE_TYPE_NAME)));
-            cursor.close();
-            return event;
+            // Service type                               
+            int serviceTypeId = cursor.getInt(cursor.getColumnIndexOrThrow(Contract.Events.SERVICE_TYPE_ID));
+            String serviceTypeName = cursor.getString(cursor.getColumnIndexOrThrow(Contract.Events.SERVICE_TYPE_NAME));
+            TextView serviceTypeView = (TextView) getActivity().findViewById(R.id.event_service_type);
+            serviceTypeView.setText(serviceTypeName + " (#" + serviceTypeId + ")");
         }
         cursor.close();
-        return null;
     }
 
 }
