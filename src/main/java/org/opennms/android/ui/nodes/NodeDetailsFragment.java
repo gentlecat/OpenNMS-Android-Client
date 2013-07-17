9fa66b9f4d43bb102f03283f41382b1f2223c386
Roman Tsukanov <roman@tsukanov.me>
2013-07-18 01:31:32 +1000

    Removed Node class. No longer creating unnessasary object.

--- src/main/java/org/opennms/android/ui/nodes/NodeDetailsFragment.java
+++ src/main/java/org/opennms/android/ui/nodes/NodeDetailsFragment.java
@@ -10,7 +10,6 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import com.actionbarsherlock.app.SherlockFragment;
 import org.opennms.android.R;
-import org.opennms.android.dao.Node;
 import org.opennms.android.provider.Contract;
 
 import java.text.ParseException;
@@ -19,7 +18,6 @@ import java.util.Date;
 
 public class NodeDetailsFragment extends SherlockFragment {
     private static final String TAG = "NodeDetailsFragment";
-    private Node node;
     private long nodeId;
 
     // Do not remove
@@ -38,68 +36,56 @@ public class NodeDetailsFragment extends SherlockFragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        updateContent();
+        updateContent(nodeId);
     }
 
-    public void updateContent() {
-        node = getNode(nodeId);
+    public void updateContent(long nodeId) {
+        Cursor cursor = getActivity().getContentResolver().query(
+                Uri.withAppendedPath(Contract.Nodes.CONTENT_URI, String.valueOf(nodeId)),
+                null, null, null, null);
+        if (cursor.moveToFirst()) {
+            String name = cursor.getString(cursor.getColumnIndexOrThrow(Contract.Nodes.NAME));
+            TextView nameView = (TextView) getActivity().findViewById(R.id.node_name);
+            nameView.setText(name);
 
-        if (node != null) {
-            TextView name = (TextView) getActivity().findViewById(R.id.node_name);
-            name.setText(node.getName());
+            int id = cursor.getInt(cursor.getColumnIndexOrThrow(Contract.Nodes._ID));
+            TextView idView = (TextView) getActivity().findViewById(R.id.node_id);
+            idView.setText(String.valueOf(id));
 
-            TextView id = (TextView) getActivity().findViewById(R.id.node_id);
-            id.setText(String.valueOf(node.getId()));
-
-            TextView contactText = (TextView) getActivity().findViewById(R.id.node_contact);
-            contactText.setText(node.getSysContact());
+            String sysContact = cursor.getString(cursor.getColumnIndexOrThrow(Contract.Nodes.SYS_CONTACT));
+            TextView sysContactView = (TextView) getActivity().findViewById(R.id.node_contact);
+            sysContactView.setText(sysContact);
 
             TextView timeView = (TextView) getActivity().findViewById(R.id.node_creation_time);
             // Example: "2011-09-27T12:15:32.363-04:00"
             SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
-            String timeString = node.getCreateTime();
-            if (timeString != null) {
+            String createdTime = cursor.getString(cursor.getColumnIndexOrThrow(Contract.Nodes.CREATED_TIME));
+            if (createdTime != null) {
                 try {
-                    Date createTime = format.parse(timeString);
+                    Date createTime = format.parse(createdTime);
                     timeView.setText(createTime.toString());
                 } catch (ParseException e) {
                     Log.e(TAG, "Creation time parsing error");
                 }
             }
 
-            TextView labelSource = (TextView) getActivity().findViewById(R.id.node_label_source);
-            labelSource.setText(node.getLabelSource());
+            String labelSource = cursor.getString(cursor.getColumnIndexOrThrow(Contract.Nodes.LABEL_SOURCE));
+            TextView labelSourceView = (TextView) getActivity().findViewById(R.id.node_label_source);
+            labelSourceView.setText(labelSource);
 
-            TextView type = (TextView) getActivity().findViewById(R.id.node_type);
-            type.setText(node.getType());
+            String type = cursor.getString(cursor.getColumnIndexOrThrow(Contract.Nodes.TYPE));
+            TextView typeView = (TextView) getActivity().findViewById(R.id.node_type);
+            typeView.setText(type);
 
-            TextView description = (TextView) getActivity().findViewById(R.id.node_description);
-            description.setText(node.getDescription());
+            String desc = cursor.getString(cursor.getColumnIndexOrThrow(Contract.Nodes.DESCRIPTION));
+            TextView descView = (TextView) getActivity().findViewById(R.id.node_description);
+            descView.setText(desc);
 
-            TextView location = (TextView) getActivity().findViewById(R.id.node_location);
-            location.setText(node.getLocation());
-        }
-    }
-
-    private Node getNode(long id) {
-        Cursor cursor = getActivity().getContentResolver().query(
-                Uri.withAppendedPath(Contract.Nodes.CONTENT_URI, String.valueOf(id)),
-                null, null, null, null
-        );
-        if (cursor.moveToFirst()) {
-            Node node = new Node((cursor.getInt(cursor.getColumnIndexOrThrow(Contract.Nodes._ID))));
-            node.setType(cursor.getString(cursor.getColumnIndexOrThrow(Contract.Nodes.TYPE)));
-            node.setName(cursor.getString(cursor.getColumnIndexOrThrow(Contract.Nodes.NAME)));
-            node.setCreateTime(cursor.getString(cursor.getColumnIndexOrThrow(Contract.Nodes.CREATED_TIME)));
-            node.setSysContact(cursor.getString(cursor.getColumnIndexOrThrow(Contract.Nodes.SYS_CONTACT)));
-            node.setLabelSource(cursor.getString(cursor.getColumnIndexOrThrow(Contract.Nodes.LABEL_SOURCE)));
-            node.setLocation(cursor.getString(cursor.getColumnIndexOrThrow(Contract.Nodes.LOCATION)));
-            node.setDescription(cursor.getString(cursor.getColumnIndexOrThrow(Contract.Nodes.DESCRIPTION)));
-            cursor.close();
-            return node;
+            String location = cursor.getString(cursor.getColumnIndexOrThrow(Contract.Nodes.LOCATION));
+            TextView locationView = (TextView) getActivity().findViewById(R.id.node_location);
+            locationView.setText(location);
         }
         cursor.close();
-        return null;
     }
 
 }
