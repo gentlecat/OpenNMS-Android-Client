61f108e2ae49dc5b1dd9af65d476a92dcc951b77
Roman Tsukanov <roman@tsukanov.me>
2013-08-14 14:30:36 +1000

    Warning icon is now dispalyed in nodes list items when node is related to any alarm. Closes #27.

--- src/main/java/org/opennms/android/ui/nodes/NodeAdapter.java
+++ src/main/java/org/opennms/android/ui/nodes/NodeAdapter.java
@@ -2,21 +2,27 @@ package org.opennms.android.ui.nodes;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.support.v4.widget.CursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.opennms.android.R;
 import org.opennms.android.provider.Contract;
+import org.opennms.android.provider.DatabaseHelper;
 
 public class NodeAdapter extends CursorAdapter {
 
     private LayoutInflater layoutInflater;
+    private SQLiteDatabase db;
 
     public NodeAdapter(Context context, Cursor cursor, int flags) {
         super(context, cursor, flags);
+        db = new DatabaseHelper(context).getReadableDatabase();
         layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
@@ -42,6 +48,7 @@ public class NodeAdapter extends CursorAdapter {
             viewHolder = new ViewHolder();
             viewHolder.id = (TextView) convertView.findViewById(R.id.node_list_item_id);
             viewHolder.name = (TextView) convertView.findViewById(R.id.node_list_item_name);
+            viewHolder.warning = (ImageView) convertView.findViewById(R.id.node_list_item_warning);
         } else {
             viewHolder = (ViewHolder) convertView.getTag();
         }
@@ -52,12 +59,25 @@ public class NodeAdapter extends CursorAdapter {
         String name = mCursor.getString(mCursor.getColumnIndexOrThrow(Contract.Nodes.NAME));
         viewHolder.name.setText(name);
 
+        if (anyAlarms(id)) {
+            viewHolder.warning.setVisibility(View.VISIBLE);
+        }
+
         return convertView;
     }
 
+    private boolean anyAlarms(int nodeId) {
+        SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
+        queryBuilder.setTables(Contract.Tables.ALARMS);
+        queryBuilder.appendWhere(Contract.Alarms.NODE_ID + "=" + nodeId);
+        return queryBuilder.query(db, null, null, null, null, null, null).moveToFirst();
+    }
+
     static class ViewHolder {
+
         TextView id;
         TextView name;
+        ImageView warning;
     }
 
 }
