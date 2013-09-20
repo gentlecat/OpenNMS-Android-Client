bc5a600b10986dde14f46d350f62f195b9fe56dd
Roman Tsukanov <roman@tsukanov.me>
2013-09-21 09:11:40 +1000

    Added more methods into LoadManager.

--- src/main/java/org/opennms/android/net/DataLoader.java
+++ src/main/java/org/opennms/android/net/DataLoader.java
@@ -4,6 +4,9 @@ import android.content.Context;
 
 import java.io.IOException;
 
+/**
+ * Helper-class that can be used to access OpenNMS server.
+ */
 public class DataLoader {
 
     private Client serverCommunication;
@@ -20,16 +23,60 @@ public class DataLoader {
         return serverCommunication.get(String.format("nodes?orderBy=id&limit=%d&offset=%d&comparator=ilike&label=%s%%25", limit, offset, searchQuery));
     }
 
+    /**
+     * Get a specific node
+     *
+     * @param nodeId Node ID.
+     * @return Server response.
+     * @throws IOException
+     */
+    public Response node(int nodeId) throws IOException {
+        return serverCommunication.get(String.format("nodes/%d", nodeId));
+    }
+
     public Response events(int limit, int offset) throws IOException {
         return serverCommunication.get(String.format("events?orderBy=id&order=desc&limit=%d&offset=%d", limit, offset));
+    }
+
+    /**
+     * Get a specific event
+     *
+     * @param eventId Event ID.
+     * @return Server response.
+     * @throws IOException
+     */
+    public Response event(int eventId) throws IOException {
+        return serverCommunication.get(String.format("events/%d", eventId));
     }
 
     public Response alarms(int limit, int offset) throws IOException {
         return serverCommunication.get(String.format("alarms?limit=%d&offset=%d", limit, offset));
     }
 
+    /**
+     * Get a specific alarm.
+     *
+     * @param alarmId Alarm ID.
+     * @return Server response.
+     * @throws IOException
+     */
+    public Response alarm(int alarmId) throws IOException {
+        return serverCommunication.get(String.format("alarms/%d", alarmId));
+    }
+
     public Response outages(int limit, int offset) throws IOException {
         return serverCommunication.get(String.format("outages?limit=%d&offset=%d", limit, offset));
+    }
+
+    /**
+     * Get a specific outage.
+     *
+     * @param outageId Outage ID.
+     * @return Server response.
+     * @throws IOException
+     */
+    public Response outage(int outageId) throws IOException {
+        return serverCommunication.get(String.format("outages/%d", outageId));
     }
 
     public Response user(String name) throws IOException {
