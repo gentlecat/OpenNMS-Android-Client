058c620675e30dd7f4a92a0396590e54801f29fd
Roman Tsukanov <roman@tsukanov.me>
2014-01-12 19:22:01 +1000

    Removed useless fragment from TitleActivity.

--- src/main/java/org/opennms/android/ui/TitleActivity.java
+++ src/main/java/org/opennms/android/ui/TitleActivity.java
@@ -1,12 +1,10 @@
 package org.opennms.android.ui;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -21,43 +19,32 @@ public class TitleActivity extends ActionBarActivity {
 
         // TODO: Add is_completed flag. If true, skip title screen and launch base activity.
 
-        setContentView(R.layout.activity_title);
         getSupportActionBar().hide();
+        final Activity thisActivity = this;
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().add(R.id.container, new TitleFragment()).commit();
-        }
-    }
+        setContentView(R.layout.activity_title);
 
-    public static class TitleFragment extends Fragment {
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_title, container, false);
+        Button configure = (Button) findViewById(R.id.configure_button);
+        configure.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Fix: After settings are closed, app is not returning to base activity.
+                Intent intent = new Intent(thisActivity, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
 
-            Button configure = (Button) rootView.findViewById(R.id.configure_button);
-            configure.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // TODO: Fix: After settings are closed, app is not returning to base activity.
-                    Intent intent = new Intent(getActivity(), SettingsActivity.class);
-                    startActivity(intent);
-                }
-            });
-
-            Button demo = (Button) rootView.findViewById(R.id.demo_button);
-            demo.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Toast.makeText(getActivity(),
-                            getString(R.string.title_activity_tip_toast),
-                            Toast.LENGTH_LONG).show();
-                    Intent intent = new Intent(getActivity(), NodesActivity.class);
-                    startActivity(intent);
-                }
-            });
-
-            return rootView;
-        }
+        Button demo = (Button) findViewById(R.id.demo_button);
+        demo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(thisActivity,
+                        getString(R.string.title_activity_tip_toast),
+                        Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(thisActivity, NodesActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
