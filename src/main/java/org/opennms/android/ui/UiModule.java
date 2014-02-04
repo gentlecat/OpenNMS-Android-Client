11ca08e67b7af2483b8cca2589a241154154201b
Roman Tsukanov <roman@tsukanov.me>
2014-02-05 04:15:18 +1000

    Added missing injections. Doh.

--- src/main/java/org/opennms/android/ui/UiModule.java
+++ src/main/java/org/opennms/android/ui/UiModule.java
@@ -2,15 +2,19 @@ package org.opennms.android.ui;
 
 import org.opennms.android.sync.AlarmsSyncAdapter;
 import org.opennms.android.sync.LoadManager;
+import org.opennms.android.ui.alarms.AlarmDetailsActivity;
 import org.opennms.android.ui.alarms.AlarmDetailsFragment;
 import org.opennms.android.ui.alarms.AlarmsActivity;
 import org.opennms.android.ui.alarms.AlarmsListFragment;
+import org.opennms.android.ui.events.EventDetailsActivity;
 import org.opennms.android.ui.events.EventDetailsFragment;
 import org.opennms.android.ui.events.EventsActivity;
 import org.opennms.android.ui.events.EventsListFragment;
+import org.opennms.android.ui.nodes.NodeDetailsActivity;
 import org.opennms.android.ui.nodes.NodeDetailsFragment;
 import org.opennms.android.ui.nodes.NodesActivity;
 import org.opennms.android.ui.nodes.NodesListFragment;
+import org.opennms.android.ui.outages.OutageDetailsActivity;
 import org.opennms.android.ui.outages.OutageDetailsFragment;
 import org.opennms.android.ui.outages.OutagesActivity;
 import org.opennms.android.ui.outages.OutagesListFragment;
@@ -23,23 +27,28 @@ import dagger.Module;
                 AlarmsSyncAdapter.class,
 
                 BaseActivity.class,
+                DetailsActivity.class,
                 DetailsFragment.class,
                 SettingsActivity.class,
 
                 NodesActivity.class,
-                NodeDetailsFragment.class,
                 NodesListFragment.class,
+                NodeDetailsActivity.class,
+                NodeDetailsFragment.class,
 
                 OutagesActivity.class,
                 OutagesListFragment.class,
+                OutageDetailsActivity.class,
                 OutageDetailsFragment.class,
 
                 AlarmsActivity.class,
                 AlarmsListFragment.class,
+                AlarmDetailsActivity.class,
                 AlarmDetailsFragment.class,
 
                 EventsActivity.class,
                 EventsListFragment.class,
+                EventDetailsActivity.class,
                 EventDetailsFragment.class,
         },
         complete = false,
